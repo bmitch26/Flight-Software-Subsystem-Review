d1623c3121c1bbdc2ca86fd8d4e917c7f8e644e4
Brett <brtm23@gmail.com>
2025-05-25 00:35:58 -0500

    bitfield example

---
+++ bitfields.cpp
@@ -0,0 +1,23 @@
+/*
+Bitfields: controlling memory at the bit level
+*/
+
+#include <iostream>
+using namespace std;
+
+struct Flags {
+    unsigned int a : 1;  // 1 bit
+    unsigned int b : 3;  // 3 bits
+    unsigned int c : 4;  // 4 bits
+};
+
+int main() {
+    Flags f;
+    f.a = 1;
+    f.b = 5;  // up to 7 (3 bits max)
+    f.c = 15; // up to 15 (4 bits max)
+
+    cout << "a: " << f.a << ", b: " << f.b << ", c: " << f.c << endl;
+    cout << "Size of Flags struct: " << sizeof(Flags) << endl;
+    return 0;
+}
