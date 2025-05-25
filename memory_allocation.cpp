aa6f17ea76560677f4daf11239c2bf3fce16dbf0
Brett <brtm23@gmail.com>
2025-05-24 23:33:32 -0500

    example 2 added

--- memory_allocation.cpp
+++ memory_allocation.cpp
@@ -7,22 +7,36 @@ than the stack.
 Useful when we don't known how much memory we will need. This 
 makes our programs more flexible, especially when accepting
 user input.
+
+To compile:
+g++ -g memory_allocation.cpp -o memory_allocation
+./memory_allocation
 */
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    int *arr = new int[5]; // allocate array of 5 integers
+    // Example 1
+    // int *arr = new int[5]; // allocate array of 5 integers
 
-    for (int i = 0; i < 5; i++) {
-        arr[i] = i * 10;
-    }
+    // for (int i = 0; i < 5; i++) {
+    //     arr[i] = i * 10;
+    // }
 
-    for (int i = 0; i < 5; i++) {
-        cout << arr[i] << " ";
-    }
+    // for (int i = 0; i < 5; i++) {
+    //     cout << arr[i] << " ";
+    // }
 
-    delete[] arr; // note the [] here for arrays!!
+    // delete[] arr; // note the [] here for arrays!!
+
+    // Example 2
+    int *pNum = NULL;
+    pNum = new int;
+    *pNum = 21;
+    cout << "address: " << pNum << "\n";
+    cout << "value: " << pNum << "\n";
+    delete pNum;
+
     return 0;
 }
